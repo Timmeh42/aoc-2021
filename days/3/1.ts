9e34fd79b8de549cc23868b78ca2a94c2a293de3
Timmeh42 <tmeier42+github@gmail.com>
2021-12-03 17:46:20 +0200

    faster

--- days/3/1.ts
+++ days/3/1.ts
@@ -2,19 +2,19 @@ module.exports = function (input: string) {
     const lines = input.trim().split('\n');
     const lineCount = lines.length;
     const lineLength = lines[0].length;
-    const bits: number[] = (new Array(lines[0].length)).fill(0);
-    for (let i = 0; i < lineCount; i++) {
-        const line = lines[i];
-        for (let j = 0; j < lineLength; j++) {
-            const bit = line[j];
-            if (bit === '1') {
-                bits[j]++;
+    let gamma = 0;
+    for (let j = 0; j < lineLength; j++) {
+        gamma = gamma << 1;
+        let counter = 0;
+        for (let i = 0; i < lineCount; i++) {
+            if (lines[i][j] === '1') {
+                counter++
+            }
+            if (counter * 2 >= lineCount) {
+                gamma += 1;
+                break;
             }
         }
     }
-    for (let i = 0; i < lineLength; i++) {
-        bits[i] = Math.round(bits[i] / lineCount);
-    }
-    const gamma = parseInt(bits.join(''), 2);
     return gamma * ((2 ** lineLength - 1) ^ gamma);
 }
