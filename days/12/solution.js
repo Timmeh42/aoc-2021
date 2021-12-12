8c6bbd01f98cb0c7f1cfbf37257594bc26beca7c
timmeh42 <tmeier42+github@gmail.com>
2021-12-12 16:46:27 +0200

    faster (mostly because of not tracking exact path)

--- days/12/solution.js
+++ days/12/solution.js
@@ -4,51 +4,46 @@ module.exports = function (input) {
 
     const map = new Map();
     for (let line of lines) {
-        if (!map.has(line[0])) {
-            map.set(line[0], {
-                big: line[0] === line[0].toUpperCase(),
-                links: [],
-            });
-        }
-        if (!map.has(line[1])) {
-            map.set(line[1], {
-                big: line[1] === line[1].toUpperCase(),
-                links: [],
-            });
-        }
-        map.get(line[0]).links.push(line[1]);
-        map.get(line[1]).links.push(line[0]);
+        const node1 = map.get(line[0]) || {
+            label: line[0],
+            big: line[0] === line[0].toUpperCase(),
+            links: [],
+        };
+        const node2 = map.get(line[1]) || {
+            label: line[1],
+            big: line[1] === line[1].toUpperCase(),
+            links: [],
+        };
+        map.set(line[0], node1);
+        map.set(line[1], node2);
+        node1.links.push(node2);
+        node2.links.push(node1);
     }
 
-    part1 = explore('start', map, true).length;
-    part2 = explore('start', map, false).length;
+    part1 = explore(map.get('start'), map, true);
+    part2 = explore(map.get('start'), map, false);
 
     return [part1, part2];
 }
 
-function explore (node, map, seenSmall, seen = new Set()) {
-    const paths = [];
-    if (node === 'end') {
-        return ['end'];
+function explore (node, map, seenSmall, seen = []) {
+    let paths = 0;
+    if (node.label === 'end') {
+        return 1;
     }
-    const currNode = map.get(node);
-    if (!currNode.big) {
-        seen.add(node);
+    if (!node.big) {
+        seen.push(node);
     }
-    for (linkedNode of currNode.links) {
+    for (linkedNode of node.links) {
         let newSeenSmall = seenSmall;
-        if (seen.has(linkedNode)) {
-            if (newSeenSmall || linkedNode === 'start') {
+        if (seen.includes(linkedNode)) {
+            if (newSeenSmall || linkedNode.label === 'start') {
                 continue;
             } else {
                 newSeenSmall = true;
             }
         }
-        let pathsAhead = explore(linkedNode, map, newSeenSmall, new Set(seen));
-        if (pathsAhead.length > 0) {
-            pathsAhead = pathsAhead.map(p => node + ',' + p);
-            paths.push(...pathsAhead);
-        }
+        paths += explore(linkedNode, map, newSeenSmall, [...seen]);
     }
     return paths;
 }
