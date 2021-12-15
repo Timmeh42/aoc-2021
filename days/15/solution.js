09cfea7e2435858679cf61aa5a54588a245cdd44
timmeh42 <tmeier42+github@gmail.com>
2021-12-15 17:39:05 +0200

    cleaning things up stopped the memory leak?

--- days/15/solution.js
+++ days/15/solution.js
@@ -49,115 +49,9 @@ module.exports = function (input) {
     const height = lines.length;
     const grid = lines.map(y => y.split('').map(s => +s));
 
-    let goal = (width-1) + (height-1) * width;
-    let frontier = new PriorityQueue();
-    frontier.insert(0, 0);
-    let cost_so_far = new Map();
-    cost_so_far.set(0, 0);
+    part1 = dijkstra(grid, width, height);
 
-    while (frontier.length) {
-        let current = frontier.pop();
-
-        if (current === goal) {
-            break;
-        }
-
-        let x = current % width;
-        let y = Math.floor(current / width);
-
-        if (x > 0) {
-            let next = current-1;
-            let new_cost = cost_so_far.get(current) + grid[y][x-1];
-            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
-                cost_so_far.set(next, new_cost);
-                frontier.insert(next, new_cost);
-            }
-        }
-        if (y > 0) {
-            let next = current-width;
-            let new_cost = cost_so_far.get(current) + grid[y-1][x];
-            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
-                cost_so_far.set(next, new_cost);
-                frontier.insert(next, new_cost);
-            }
-        }
-        if (x < width-1) {
-            let next = current+1;
-            let new_cost = cost_so_far.get(current) + grid[y][x+1];
-            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
-                cost_so_far.set(next, new_cost);
-                frontier.insert(next, new_cost);
-            }
-        }
-        if (y < height-1) {
-            let next = current+width;
-            let new_cost = cost_so_far.get(current) + grid[y+1][x];
-            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
-                cost_so_far.set(next, new_cost);
-                frontier.insert(next, new_cost);
-            }
-        }
-    }
-
-    part1 = cost_so_far.get(goal);
-
-
-
-    bigWidth = width * 5;
-    bigHeight = height * 5;
-
-    goal = (bigWidth-1) + (bigHeight-1) * bigWidth;
-    frontier = new PriorityQueue();
-    frontier.insert(0, 0);
-    cost_so_far = new Map();
-    cost_so_far.set(0, 0);
-
-    let current;
-    while (frontier.length) {
-        current = frontier.pop();
-
-        if (current === goal) {
-            break;
-        }
-
-        let x = current % bigWidth;
-        let y = Math.floor(current / bigWidth);
-
-        if (x > 0) {
-            let next = current-1;
-            let new_cost = cost_so_far.get(current) + cost(x-1, y, width, height, grid);
-            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
-                cost_so_far.set(next, new_cost);
-                frontier.insert(next, new_cost);
-            }
-        }
-        if (y > 0) {
-            let next = current-bigWidth;
-            let new_cost = cost_so_far.get(current) + cost(x, y-1, width, height, grid);
-            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
-                cost_so_far.set(next, new_cost);
-                frontier.insert(next, new_cost);
-            }
-        }
-        if (x < bigWidth-1) {
-            let next = current+1;
-            let new_cost = cost_so_far.get(current) + cost(x+1, y, width, height, grid);
-            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
-                cost_so_far.set(next, new_cost);
-                frontier.insert(next, new_cost);
-            }
-        }
-        if (y < bigHeight-1) {
-            let next = current+bigWidth;
-            let new_cost = cost_so_far.get(current) + cost(x, y+1, width, height, grid);
-            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
-                cost_so_far.set(next, new_cost);
-                frontier.insert(next, new_cost);
-            }
-        }
-    }
-
-    part2 = cost_so_far.get(goal)
+    part2 = dijkstra(grid, width, height, 5);
 
     return [part1, part2];
 }
@@ -168,4 +62,50 @@ function cost(x, y, width, height, grid) {
     const dx = (x - nx) / width;
     const dy = (y - ny) / height;
     return (grid[ny][nx] + dx + dy - 1) % 9 + 1;
+}
+
+function dijkstra (grid, width, height, scale = 1) {
+    
+    bigWidth = width * scale;
+    bigHeight = height * scale;
+
+    goal = (bigWidth-1) + (bigHeight-1) * bigWidth;
+    frontier = new PriorityQueue();
+    frontier.insert(0, 0);
+    cost_so_far = new Map();
+    cost_so_far.set(0, 0);
+
+    while (frontier.length) {
+        let current = frontier.pop();
+
+        if (current === goal) {
+            break;
+        }
+
+        let x = current % bigWidth;
+        let y = Math.floor(current / bigWidth);
+
+        let neighbours = [];
+        if (x > 0) {
+            neighbours.push([-1, 0])
+        }
+        if (y > 0) {
+            neighbours.push([0, -1])
+        }
+        if (x < bigWidth-1) {
+            neighbours.push([+1, 0])
+        }
+        if (y < bigHeight-1) {
+            neighbours.push([0, +1])
+        }
+        for (let [dx, dy] of neighbours) {
+            const next = current + dx + dy * bigWidth;
+            const new_cost = cost_so_far.get(current) + cost(x + dx, y + dy, width, height, grid);
+            if (!cost_so_far.has(next) || new_cost < cost_so_far.get(next)) {
+                cost_so_far.set(next, new_cost);
+                frontier.insert(next, new_cost);
+            }
+        }
+    }
+    return cost_so_far.get(goal);
 }
