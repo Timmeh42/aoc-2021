98c2c63cbc73293d32fdce8b2e781c3346e9b00f
timmeh42 <tmeier42+github@gmail.com>
2021-12-17 21:53:01 +0200

    a faster method

--- days/17/solution.js
+++ days/17/solution.js
@@ -5,28 +5,45 @@ module.exports = function (input) {
     part1 = (y1 + 1) * y1 / 2;
 
     part2 = 0;
+
+    let vels = new Set();
+
+    let ySteps = new Map();
+    let yStepsMax = -Infinity;
+    for (let vy = -y1; vy >= y1; vy--) {
+        for (let steps = 0, y = 0; y >= y1; y += vy - steps, steps++) {
+            if (y >= y1 && y <= y2) {
+                yStepsMax = Math.max(yStepsMax, steps);
+                const vys = ySteps.get(steps);
+                if (vys === undefined) {
+                    ySteps.set(steps, [vy]);
+                } else {
+                    vys.push(vy);
+                }
+            }
+        }
+    }
+
     for (let vx = 0; vx <= x2; vx++) {
 
         if ((vx + 1) * vx / 2 < x1) {
             continue;
         }
 
-        nextVY:
-        for (let vy = y1; vy <= -y1; vy++) {
-            for (let steps = 0, x = 0; steps <= vx && x <= x2; steps++) {
-                if (x >= x1 && x <= x2) {
-                    for (let step = 0, y = 0; steps === vx ? y >= y1 : step < steps; step++) {
-                        y += vy - step;
-                        if (step >= steps-1 && y >= y1 && y <= y2) {
-                            part2 += 1;
-                            continue nextVY;
-                        }
+        for (let steps = 0, x = 0; x <= x2 && steps <= yStepsMax; steps++) {
+            if (x >= x1 && x <= x2) {
+                const vys = ySteps.get(steps);
+                if (vys !== undefined) {
+                    for (let i = 0; i < vys.length; i++) {
+                        vels.add(vx + vys[i] * x2);
                     }
                 }
+            }
+            if (vx > steps) {
                 x += vx - steps;
             }
         }
-
     }
+    part2 = vels.size;
     return [part1, part2];
 }
