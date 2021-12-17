78475bdb3b7fd7ae233a6ca467737ca596f6c66e
timmeh42 <tmeier42+github@gmail.com>
2021-12-17 20:07:36 +0200

    dont use a set

--- days/17/solution.js
+++ days/17/solution.js
@@ -4,29 +4,29 @@ module.exports = function (input) {
 
     part1 = (y1 + 1) * y1 / 2;
 
-    let velocities = new Set();
+    part2 = 0;
     for (let vx = 0; vx <= x2; vx++) {
 
         if ((vx + 1) * vx / 2 < x1) {
             continue;
         }
 
-        for (let steps = 0, x = 0; steps <= vx && x <= x2; steps++) {
-            if (x >= x1 && x <= x2) {
-                for (let vy = y1; vy <= -y1; vy++) {
+        nextVY:
+        for (let vy = y1; vy <= -y1; vy++) {
+            for (let steps = 0, x = 0; steps <= vx && x <= x2; steps++) {
+                if (x >= x1 && x <= x2) {
                     for (let step = 0, y = 0; steps === vx ? y >= y1 : step < steps; step++) {
                         y += vy - step;
                         if (step >= steps-1 && y >= y1 && y <= y2) {
-                            velocities.add(vx + ',' + vy);
-                            if (step > steps) break;
+                            part2 += 1;
+                            continue nextVY;
                         }
                     }
                 }
+                x += vx - steps;
             }
-            x += vx - steps;
         }
 
     }
-    part2 = velocities.size;
     return [part1, part2];
 }
